47da6a5ad8594cd0b0411d5d4994c5b1ae8e970e
Kevin Balbuena <kbalbuena5@yahoo.com>
2020-07-01 18:22:59 -0700

    finish loop practice

--- loop.js
+++ loop.js
@@ -22,9 +22,45 @@
 
 //DO while
 
-let i = 0;
+// let i = 0;
 
-do {
-  console.log(`Numbers ${i}`);
-  i++;
-} while (i <= 10);
+// do {
+//   console.log(`Numbers ${i}`);
+//   i++;
+// } while (i <= 10);
+
+//Loop Through array
+const cars = ["Ford", "Chevy", "Honda", "Toyota"];
+
+// for (let i = 0; i < cars.length; i++) {
+//   console.log(cars[i]);
+// }
+
+// For Each
+cars.forEach(function (car, index) {
+  console.log(`${index}: ${car}`);
+});
+
+// Map
+// const users = [
+//   { id: 1, name: "John" },
+//   { id: 2, name: "Sara" },
+//   { id: 3, name: "Karen" },
+//   { id: 4, name: "Steve" },
+// ];
+
+// const ids = users.map(function (user) {
+//   return user.id;
+// });
+
+// console.log(ids);
+
+const user = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 49,
+};
+
+for (let x in user) {
+  console.log(`${x} : ${user[x]}`);
+}
